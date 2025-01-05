825b29004e63594d7a7fe5e76f96851607664869
Ryanff72 <ryanff72@gmail.com>
2025-01-04 22:33:22 -0500

    improved modularity in processing acoustic indices. added a dropdown menu to select which index should be calculated.

--- eco_calc/acoustic_tools.py
+++ eco_calc/acoustic_tools.py
@@ -14,60 +14,53 @@ class AcousticTools:
 
 	# Calculates ACI based on parameters given.
 
-	def find_aci(folder, sample_rate=44100, fft_window_size=1024, hop_length=512, resolution_ms=60000, batch_size_in_file_count=20): 
+	def calculate_acoustic_index(folder, index_name, sample_rate=44100, fft_window_size=1024, hop_length=512, resolution_ms=50000, batch_size_in_file_count=1): 
 		on_file = 0
 		audio_chunk = AudioSegment.empty()
 		file_count = len(glob.glob(os.path.join(folder, "*")))
-		aci_values = []
+		index_values = []
 		start = time.time()
-
 		for file in sorted(glob.glob(os.path.join(folder, "*"))):
 			on_file += 1
 			audio_chunk += AudioSegment.from_file(file)
-
 			if on_file % batch_size_in_file_count == 0 or on_file == file_count:
 				print(f"batch {math.floor(on_file / batch_size_in_file_count)} out of {math.ceil(file_count / batch_size_in_file_count)}")
 				audio_signal, sr = AudioChunkToLibrosa.audio_chunk_to_librosa(audio_chunk)
 				chunks = AcousticTools.resolutionize(audio_signal, sr, resolution_ms)
 				totallen = 0
-
 				for chunk in chunks:
-					totallen += len(chunk)	
-					# Compute the spectrogram
-					# f: Frequencies, t: Time bins, Sxx: Spectrogram (magnitude of frequency components over time)
-					frequencies, time_bins, spectrogram_magnitude = spectrogram(
-						chunk,
-						fs=sr,
-						nperseg=fft_window_size,
-						noverlap=fft_window_size - hop_length
-					)
-
-					# Calculate amplitude changes across time bins
-					amplitude_changes = np.sum(np.abs(np.diff(spectrogram_magnitude, axis=1)), axis=1)
-
-					# Calculate total amplitude for each frequency band
-					total_band_amplitude = np.sum(spectrogram_magnitude, axis=1)
-
-					# Normalize amplitude changes by total amplitude for each frequency band
-					# Avoid division by zero using a small constant (1e-8)
-					aci_per_frequency_band = amplitude_changes / (total_band_amplitude + 1e-8)
-
-					# Compute the mean ACI across all frequency bands
-					aci = np.mean(aci_per_frequency_band)
-
-					aci_values.append(aci)
+					if index_name == "ACI":
+						index_values.append(AcousticTools.calculate_aci(sr, fft_window_size, hop_length, chunk))
 				audio_chunk = AudioSegment.empty()
 		end = time.time()
-		print(aci_values)
+		print(index_values)
 		print(f"that took {end - start} seconds.")	
-		return aci
-
-	# Combines every audio (.wav) file in a folder. Returns the resulting audio signal.
-
-	#def combine_audio(folder):
+		return index_values
 
 	# Splits an audio file into the chunks. The acoustic index of each chunk will be calculated
 	# seperately and be represented as a different data point.
+
+	def calculate_aci(sr, fft_window_size, hop_length, chunk):
+		# f: Frequencies, t: Time bins, Sxx: Spectrogram (magnitude of frequency components over time)
+		frequencies, time_bins, spectrogram_magnitude = spectrogram(
+			chunk,
+			fs=sr,
+			nperseg=fft_window_size,
+			noverlap=fft_window_size - hop_length
+		)
+
+		# Calculate amplitude changes across time bins
+		amplitude_changes = np.sum(np.abs(np.diff(spectrogram_magnitude, axis=1)), axis=1)
+
+		# Calculate total amplitude for each frequency band
+		total_band_amplitude = np.sum(spectrogram_magnitude, axis=1)
+
+		# Normalize amplitude changes by total amplitude for each frequency band
+		# Avoid division by zero using a small constant (1e-8)
+		aci_per_frequency_band = amplitude_changes / (total_band_amplitude + 1e-8)
+
+		# Compute the mean ACI across all frequency bands
+		return np.mean(aci_per_frequency_band)
 
 	def resolutionize(audio, sr, resolution_ms):
 		num_samples_per = int(sr * (resolution_ms / 1000.0))
--- eco_calc/menu.py
+++ eco_calc/menu.py
@@ -1,26 +1,35 @@
 import ctypes
 import glob
 import tkinter as tk
+from tkinter import ttk
 import platform
 from directory_operations import DirectoryOperations
 from acoustic_tools import AcousticTools
 
 class Menu:
-    def create_main_menu():
-        # Create a Window
-        root = tk.Tk()
-        root.title("Ecoacoustic_Calculator")
-        root.geometry("640x480")
+	def create_main_menu():
+		root = tk.Tk()
+		root.title("Ecoacoustic_Calculator")
+		root.geometry("640x480")
+		# Create a Window
+		# File Import Button
+		file_import_button = tk.Button(root, text="File", command=DirectoryOperations.open_folder)
+		file_import_button.pack(pady=50)
+		file_import_button.place(x=5,y=5)
 
-        # File Import Button
-        file_import_button = tk.Button(root, text="File", command=DirectoryOperations.open_folder)
-        file_import_button.pack(pady=50)
-        file_import_button.place(x=5,y=5)
+		calculate_index_button = tk.Button(root, text=f"Calculate", command=lambda: AcousticTools.calculate_acoustic_index(DirectoryOperations.target_audio_folder, acoustic_index_dropdown.get()))
+		calculate_index_button.pack(pady=50)
+		calculate_index_button.place(x=5,y=50)
+        # Calculate Index Dropdown
 
-        # Calculate ACI Button
-        calculate_aci_button = tk.Button(root, text="ACI", command=lambda: AcousticTools.find_aci(DirectoryOperations.target_audio_folder))
-        calculate_aci_button.pack(pady=50)
-        calculate_aci_button.place(x=5,y=50)
+		indices = ["ACI", "ADI"]
+		acoustic_index_dropdown = ttk.Combobox(root, values=indices)
+		acoustic_index_dropdown.pack()	
+		acoustic_index_dropdown.bind("<<ComboboxSelected>>", lambda event: Menu.update_calculate_button(event, calculate_index_button, acoustic_index_dropdown.get()))
+        # Calculate Index Button
 
-        # Event Loop
-        root.mainloop()
+		# Event Loop
+		root.mainloop()
+
+	def update_calculate_button(event, button, index):
+		button.config(text=f"Calculate {index}")
